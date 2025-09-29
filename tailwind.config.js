a97e7421e6bf9d85cfd13d13db19edbbbedb78da
[dyad] <agenciacombodigital@gmail.com>
2025-09-29 09:12:43 -0300

    [dyad] Aplicando correções de segurança críticas no site. - wrote 5 file(s) + extra files edited outside of Dyad

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./hooks/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        manrope: ['Manrope', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
