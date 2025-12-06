94c89cfe68592c8402dcb40c13ce7ee96b6a86a8
[dyad] <agenciacombodigital@gmail.com>
2025-12-05 23:35:11 -0300

    [dyad] Corrigi o carregamento da fonte Manrope movendo a importação para o index.html e simplifiquei a chave do Cloudflare Turnstile no formulário de contato para garantir a renderização. - wrote 3 file(s) + extra files edited outside of Dyad

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "52f9f63b9feb586a"; }
+/******/ 		__webpack_require__.h = function() { return "15bf9d6d448dad37"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
