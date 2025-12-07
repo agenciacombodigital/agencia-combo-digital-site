3075c8ff2711b3d0e3d69bd0afaf935b09059517
[dyad] <agenciacombodigital@gmail.com>
2025-12-06 23:41:58 -0300

    [dyad] Removi o link do X (Twitter) e corrigi o ícone SVG do Instagram no rodapé. - wrote 1 file(s) + extra files edited outside of Dyad

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "83987bb092ba4800"; }
+/******/ 		__webpack_require__.h = function() { return "fb867dbb5b8d2c27"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
