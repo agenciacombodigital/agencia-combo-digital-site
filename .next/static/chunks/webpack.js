9177acdfff48ab59607f3cf522e520ae55954db5
[dyad] <agenciacombodigital@gmail.com>
2025-12-07 09:09:56 -0300

    [dyad] Removi o link social do X (Twitter) da página de contato. - wrote 1 file(s) + extra files edited outside of Dyad

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b88e3ad3e8b91475"; }
+/******/ 		__webpack_require__.h = function() { return "4f9de8abbc2428a2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/contato.tsx
+++ pages/contato.tsx
@@ -118,7 +118,7 @@ const Contato: React.FC = () => {
                     <p><strong>Telefone:</strong> +55 (11) 99999-8888</p>
                  </div>
                  <div className="flex space-x-6">
-                    <SocialLink href="#" aria-label="Visite nosso perfil no X"><svg role="img" viewBox="0 0 24 24" className="h-6 w-6" fill="currentColor"><title>X</title><path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.931ZM17.61 20.644h2.039L6.486 3.24H4.298Z"/></svg></SocialLink>
+                    {/* Removido o link do X (Twitter) */}
                     <SocialLink href="https://www.instagram.com/agenciacombodigital/" aria-label="Visite nosso perfil no Instagram">
                         <svg role="img" viewBox="0 0 24 24" className="h-6 w-6" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                             <title>Instagram</title>
