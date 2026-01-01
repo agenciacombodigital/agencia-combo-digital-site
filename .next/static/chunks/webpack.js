4ebcf1aa64628e8f3e262774737fe822ab0e26c4
[dyad] <agenciacombodigital@gmail.com>
2026-01-01 16:58:42 -0300

    [dyad] Downgrade de versões do ecosystem Three.js para compatibilidade com React 18. - wrote 3 file(s), added @react-three/fiber@8.17.10, @react-three/drei@9.121.4, three@0.170.0 package(s) + extra files edited outside of Dyad

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f576992ae5a9a689"; }
+/******/ 		__webpack_require__.h = function() { return "df20107ff33e2963"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
